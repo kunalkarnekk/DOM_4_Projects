73ca359990955e19630d8e77a6955cdf01705087
kunal <kunalkarne2020@gmail.com>
2023-12-08 09:40:04 +0530

    BMI calculator

---
+++ BMI_Calculator/script.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+  const height= parseInt(document.querySelector('#height').value)
+
+  const weight = parseInt(document.querySelector('#weight').value)
+
+  const results = document.querySelector('#results')
+
+
+  if(height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `please give a valid height ${height}`
+    setTimeout(() => {
+        results.innerHTML = ''
+    }, 2000);
+  }
+  else if(weight === '' || weight < 0 || isNaN(weight)){
+    results.innerHTML = `please give a valid weight ${weight}`
+
+    setTimeout(() => {
+        results.innerHTML = ''
+    }, 2000);
+  }
+  else {
+    const BMI = (weight / ((height * height) / 10000)).toFixed(2)
+
+    results.innerHTML = BMI
+
+
+  }
+
+
+})
